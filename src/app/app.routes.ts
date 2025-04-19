48961c43c2859fd45e1a378632d40d52b1c869f2
Nelly5469 <nelly.delgado5469@alumnos.udg.mx>
2025-04-19 13:11:46 -0600

    first commit: crear componente de ejemplo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ExampleComponent } from './example/example.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', // localhost
+        component: ExampleComponent
+    }
+];
