1f4bf0fc7047e3ea8dd7934cc94c8f4a5cad6e26
Nelly5469 <nelly.delgado5469@alumnos.udg.mx>
2025-04-21 20:31:45 -0600

    lazy loading de HomePageComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,10 @@ import { ExampleComponent } from './example/example.component';
 export const routes: Routes = [
     {
         path: '', // localhost
-        component: ExampleComponent
+        //component: ExampleComponent
+        // LAZY LOAD THE MODULE
+        loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) //al crear este modulo debe de ser inicializado con routing
+        //en versiones mas nuevas se prefiere cargar el componente directamente:
+        //loadComponent: () => import('lazyComponent').then(c => c.LazyComponent)        
     }
 ];
